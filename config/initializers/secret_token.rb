71ffce151cb376970778fab692cfb267e35aa9da
Sam Mangum <sam@sammangum.com>
2013-05-21 03:24:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SammangumCom::Application.config.secret_token = 'd0c77ab16b248c781a9480e29fdcff73a6f9a960a76258a0b44f197163c671eac2e8198537850a73b64e68aa03b2a241cfb7255b09be3321775418fdd00e35f6'
